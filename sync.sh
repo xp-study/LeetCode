5d1694282e5071b04389198730c8d4b55f5a6a5c
xp-study <475031724@qq.com>
2022-10-06 09:36:31 +0800

    ci测试

---
+++ sync.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 进入工作目录
+cd /opt/xp-study/
+
+# 删除gitlab文件
+rm backup/.git
+
+rm backup/.drone.yml
+
+rm backup/sync.sh
+
+# 同步文件到github仓库
+cp ./backup/* ./LeeteCode/
+
+# 添加提交
+git add .
+
+# 提交
+git commit -m "同步文档"
+
+# 推送
+git push
+
